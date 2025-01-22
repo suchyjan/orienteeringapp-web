6c40a8375807efd4e8724286b7ba911a7c1fa8f3
suchyjan <suchy.jan@gmail.com>
2025-01-22 12:12:38 +0100

    separate css / js - fix google play logo size

---
+++ galleryHelper.js
@@ -0,0 +1,116 @@
+let currentIndex = 0
+let autoPlayInterval = null
+const galleryImages = document.querySelectorAll('.screenshots img')
+const playPauseOverlay = document.querySelector('.play-pause-overlay')
+const dotsContainer = document.querySelector('.dots')
+
+// Funkce pro zobrazení konkrétního obrázku
+function showImage (index) {
+  galleryImages.forEach((img, i) => {
+    img.classList.toggle('active', i === index)
+  })
+  updateDots(index)
+}
+
+// Funkce pro přepnutí na další obrázek
+function nextImage () {
+  currentIndex = (currentIndex + 1) % galleryImages.length
+  showImage(currentIndex)
+}
+
+// Spustí automatické přehrávání
+function startAutoPlay () {
+  if (!autoPlayInterval) {
+    autoPlayInterval = setInterval(nextImage, 3000)
+    showOverlay('pause')
+  }
+}
+
+// Pozastaví automatické přehrávání
+function stopAutoPlay () {
+  if (autoPlayInterval) {
+    clearInterval(autoPlayInterval)
+    autoPlayInterval = null
+    showOverlay('play')
+  }
+}
+
+// Přepínání mezi přehráváním a pozastavením
+function togglePlayPause () {
+  if (autoPlayInterval) {
+    stopAutoPlay()
+  } else {
+    // Před spuštěním autoPlay posunout index na další obrázek
+    currentIndex = (currentIndex + 1) % galleryImages.length
+    showImage(currentIndex) // Ukázat nový obrázek
+    startAutoPlay() // Spustit autoPlay po přepnutí
+  }
+}
+
+function showOverlay (type) {
+  const svgPlay = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path fill="#fff" d="M8 5v14l11-7z"/></svg>`
+
+  const svgPause = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path fill="#fff" d="M6 6h4v12H6zm8 0h4v12h-4z"/></svg>`
+  playPauseOverlay.innerHTML = `<span>${
+    type === 'pause' ? svgPlay : svgPause
+  }</span>`
+  playPauseOverlay.className = `play-pause-overlay ${type}`
+  playPauseOverlay.style.opacity = '1'
+
+  setTimeout(() => {
+    playPauseOverlay.style.opacity = '0'
+  }, 1000)
+}
+
+// Aktualizace indikátorů (tečky)
+function updateDots (index) {
+  const dots = document.querySelectorAll('.dot')
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index)
+  })
+}
+
+// Paticka se zobrazuje při scrollování nebo pokud není co scrollovat
+const footer = document.getElementById('footer')
+
+function checkFooterVisibility () {
+  const bodyHeight = document.body.offsetHeight
+  const windowHeight = window.innerHeight
+  if (bodyHeight <= windowHeight) {
+    footer.classList.add('show')
+  } else if (window.innerHeight + window.scrollY >= bodyHeight) {
+    footer.classList.add('show')
+  } else {
+    footer.classList.remove('show')
+  }
+}
+
+// Události pro klikání na tečky a obrázky
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('scroll', checkFooterVisibility)
+  window.addEventListener('resize', checkFooterVisibility)
+  checkFooterVisibility()
+
+  // Zobrazení prvního obrázku a teček
+  showImage(currentIndex)
+
+  galleryImages.forEach(img => {
+    img.addEventListener('click', togglePlayPause)
+  })
+
+  galleryImages.forEach((_, i) => {
+    const dot = document.createElement('div')
+    dot.classList.add('dot')
+    dot.addEventListener('click', () => {
+      currentIndex = i
+      showImage(i)
+      stopAutoPlay() // Zastaví přehrávání
+    })
+    dotsContainer.appendChild(dot)
+  })
+
+  updateDots(currentIndex)
+  startAutoPlay() // Automatické přehrávání při načtení
+})
---
+++ langHelper.js
@@ -0,0 +1,48 @@
+// Detekce preferovaného jazyka uživatele
+const userLanguage = navigator.language || navigator.languages[0] || 'en-US'
+
+// Seznam podporovaných jazyků a jejich cesty
+const supportedLanguages = ['cs', 'en', 'sv', 'fi']
+const languagePaths = {
+  cs: '/',
+  en: '/en',
+  sv: '/sv',
+  fi: '/fi'
+}
+
+// Zjištění hlavního jazyka a přiřazení správné cesty
+const primaryLanguage = userLanguage.split('-')[0].toLowerCase()
+const redirectPath = supportedLanguages.includes(primaryLanguage)
+  ? languagePaths[primaryLanguage]
+  : languagePaths['en'] // Defaultně angličtina
+
+// Získání aktuální cesty
+const currentPath = window.location.pathname
+
+// Přesměrování pouze, pokud aktuální cesta není správná
+if (currentPath !== redirectPath) {
+  const newUrl = `${window.location.origin}${redirectPath}`
+  // DO NOT REDIRECT NOW
+  //window.location.replace(newUrl); // Přesměrování na správnou URL
+}
+
+// Získání doplňkového jazyka (část za pomlčkou), pokud existuje
+const languageVariant =
+  userLanguage.split('-')[1]?.toLowerCase() || primaryLanguage
+
+// Generování správné URL pro App Store s parametrem 'l' podle detekovaného jazyka
+const appStoreUrl = `https://apps.apple.com/${languageVariant}/app/id6553980733?l=${primaryLanguage}`
+
+// Funkce pro aktualizaci href odkazu
+const updateAppStoreLink = () => {
+  const appStoreLink = document.querySelector('.app-store')
+  if (appStoreLink) {
+    appStoreLink.href = appStoreUrl // Aktualizuj href odkazu
+  } else {
+    // Pokud odkaz ještě neexistuje, opakuj pokus po krátké pauze
+    setTimeout(updateAppStoreLink, 100) // Zkusíme to znovu po 100ms
+  }
+}
+
+// Zavoláme funkci pro aktualizaci odkazu, jakmile je DOM připraven
+document.addEventListener('DOMContentLoaded', updateAppStoreLink)
